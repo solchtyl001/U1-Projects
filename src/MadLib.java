96af1541528e947b45c5f55beae6beae42b4b9e6
solchtyl001 <solchtyl001@warren.k12.in.us>
2021-08-31 13:20:00 -0400

    projects

--- src/MadLib.java
+++ src/MadLib.java
@@ -1,6 +1,48 @@
+import java.util.Scanner;
 public class MadLib {
 
     /*Create your own MadLibs. (https://www.madtakes.com/) You may use a story or example from the
     website if you don't wish to make up your own. This program must run entirely in the console.
      */
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        String noun;
+        String verb;
+        String nounTwo;
+        String verbTwo;
+
+
+        System.out.println("Enter a noun.");
+        noun = read.next();
+
+        System.out.println("Enter a verb");
+        verb = read.next();
+
+        System.out.println("Enter a noun");
+        nounTwo = read.next();
+
+        System.out.println("Enter a verb");
+        verbTwo = read.next();
+
+        System.out.println("Once upon a time there was a little " + noun + ". The " + noun + " was very " + verb + ". Then the " + noun + " met the "
+         + nounTwo + ". The " + nounTwo + " was sorta " + verbTwo + ".");
+        System.out.println( "The " + noun + " and the " + nounTwo + " fell in love and lived happily ever after. The End.");
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
--- src/RPGCharacter.java
+++ src/RPGCharacter.java
@@ -1,3 +1,7 @@
+import sun.security.provider.ConfigFile;
+
+import javax.swing.*;
+
 public class RPGCharacter {
 
     /*Create a program that calculates an RPG Character's battle stats based on the character's physical stats.
@@ -10,4 +14,55 @@ public class RPGCharacter {
 
      The program should run using dialog boxes and the output should show ALL the character's stats including a name.
      */
+    public static void main(String[] args) {
+        String Strength;
+        String Dexterity;
+        String Spirit;
+        String Wisdom;
+        String luck;
+        String firstName;
+        String Constitution;
+        String Charisma;
+        int damage;
+        int armor;
+        int spellStrength;
+        int mana;
+
+
+
+
+
+
+        firstName = JOptionPane.showInputDialog("enter a name");
+        Strength = JOptionPane.showInputDialog("enter Strength");
+        Dexterity = JOptionPane.showInputDialog("enter Dexterity");
+        Spirit = JOptionPane.showInputDialog("enter Spirit");
+        Wisdom = JOptionPane.showInputDialog("enter Wisdom");
+        Constitution = JOptionPane.showInputDialog("enter Constitution");
+        Charisma = JOptionPane.showInputDialog("enter Charisma");
+
+       
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
